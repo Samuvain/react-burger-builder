22e8e742efcdff077310f03c083651c8159c73d9
samu.vepsalainen <samu.vepsalainen@positiveproductions.fi>
2018-05-08 17:46:28 +0300

    burger builder

---
+++ src/store/actions/order.js
@@ -0,0 +1,82 @@
+import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
+
+
+export const purchaseBurgerSuccess = (id, orderData) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_SUCCESS,
+        orderId: id,
+        orderData
+    }
+};
+
+export const purchaseBurgerFail = (error) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_FAIL,
+        error
+    }
+};
+
+export const purchaseBurgerStart = () => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_START
+    }
+}
+
+export const purchaseBurger = (orderData, token) => {
+    return async dispatch => {
+        dispatch(purchaseBurgerStart());
+        try {
+            const response = await axios.post(`/orders.json?auth=${token}`, orderData);
+            dispatch(purchaseBurgerSuccess(response.data.name, orderData));
+        } catch (error) {
+            dispatch(purchaseBurgerFail(error));
+        }        
+    };
+};
+
+export const purchaseInit = () => {
+    return {
+        type: actionTypes.PURCHASE_INIT
+    }
+}
+
+export const fetchOrdersSuccess = (orders) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        orders
+    };
+};
+
+export const fetchOrdersFail = (error) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_FAIL,
+        error
+    };
+};
+
+export const fetchOrdersStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START
+    };
+};
+
+export const fetchOrders = (token, userId) => {
+    return async dispatch => {
+        dispatch(fetchOrdersStart());
+        const queryParams = `?auth=${token}&orderBy="userId"&equalTo="${userId}"`
+        try {
+            const res = await axios.get(`/orders.json${queryParams}`);
+            const fetchedOrders = [];
+            for (let key in res.data) {
+                fetchedOrders.push({
+                    ...res.data[key], 
+                    id: key
+                });
+            }
+            dispatch(fetchOrdersSuccess(fetchedOrders));
+        } catch (err) {
+            dispatch(fetchOrdersFail(err));
+        }
+    }
+};
